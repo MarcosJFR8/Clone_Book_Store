0d8e434eeb67a95f7c99aad088a612633fb1a9e1
Marcos <marcosjfr3@gmail.com>
2024-08-01 15:45:07 -0400

    book catalogue added like cards

--- books/views.py
+++ books/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from .models import Book
 from django.views.generic import ListView
+from .models import Book
 
 # Create your views here.
 class BookListView(ListView):
     model = Book
     template_name = 'books/book_list.html'
+
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'books/book_list.html', {'books': books})
